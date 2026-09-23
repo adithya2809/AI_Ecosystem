d35cb3bf6a2ba730a3c43d739d24bb03c173bddf
adithya2809 <agneyadityavardhan@gmail.com>
2026-09-23 13:17:03 +0530

    Refactor Pinecone index creation and update vector upsert logic

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 import os
 from dotenv import load_dotenv
 from pinecone import Pinecone,ServerlessSpec
-
+from gemini_client import text,embedding
 load_dotenv()
 
 pc=Pinecone(
     api_key=os.getenv("PINECONE_API_KEY")
 )
-index_name="company-policies"
+""" index_name="company-policies"
 pc.create_index(
     name=index_name,
     dimension=768,
@@ -16,5 +16,20 @@ pc.create_index(
         cloud="aws",
         region="us-east-1"
     )
-)
+) """
 print(pc.list_indexes())
+
+index=pc.Index("company-policies")
+
+index.upsert(
+    vectors=[
+        {
+            "id":"chunk_001",
+            "values": embedding,
+            "metadata":{
+                "text":text
+            }
+        }
+    ]
+)
+print("vector upserted successfully")
